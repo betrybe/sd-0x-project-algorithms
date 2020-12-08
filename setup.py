c3b7160e924fbd432a3889e914d0d893250edcfa
“brunobatista” <brunobatista101@gmail.com>
2020-12-08 13:55:38 -0300

    configuracoes iniciais

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(
+    name="inventory_report",
+    description="Projeto Inventory Report",
+    setup_requires=["pytest-runner"],
+    tests_require=["pytest"]
+)
