801af037fa32f7a5e40db8035b7e9bd96cfb3048
“brunobatista” <brunobatista101@gmail.com>
2020-12-08 15:30:44 -0300

    adicionado asseert requisito 2

--- tests/test_palindromes_recursive.py
+++ tests/test_palindromes_recursive.py
@@ -3,18 +3,18 @@ from challenges.challenge_palindromes_recursive import is_palindrome_recursive
 
 def test_validar_se_a_palavra_e_um_palindromo_retorna_true():
     word = "ANA"
-    print(is_palindrome_recursive(word, 0, len(word) - 1)) is True
+    assert is_palindrome_recursive(word, 0, len(word) - 1) is True
     word = "SOCOS"
-    print(is_palindrome_recursive(word, 0, len(word) - 1)) is True
+    assert is_palindrome_recursive(word, 0, len(word) - 1) is True
     word = "REVIVER"
-    print(is_palindrome_recursive(word, 0, len(word) - 1)) is True
+    assert is_palindrome_recursive(word, 0, len(word) - 1) is True
 
 
 def test_validar_se_a_palavra_nao_e_um_palindromo_retorna_false():
     word = "AGUA"
-    print(is_palindrome_recursive(word, 0, len(word) - 1)) is False
+    assert is_palindrome_recursive(word, 0, len(word) - 1) is False
 
 
 def test_validar_se_nao_passar_palavra_retorna_false():
     word = ""
-    print(is_palindrome_recursive(word, 0, len(word) - 1)) is False
+    assert is_palindrome_recursive(word, 0, len(word) - 1) is False
